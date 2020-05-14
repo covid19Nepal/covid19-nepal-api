dd573b89e84a3158d4b37fc0598d4fbb359e49d9
Rupesh Shrestha <5143086-rupeshshrestha722@users.noreply.gitlab.com>
2020-05-14 10:51:44 +0545

    add recovered, active and deceased

--- raw_data-to-state_district_wise_data.js
+++ raw_data-to-state_district_wise_data.js
@@ -21,11 +21,11 @@ try {
     if(!acc[stateName].districtData[districtName]) {
       
       acc[stateName].districtData[districtName] = {
-//         active: 0,
+        active: 0,
         confirmed: 0,
-//         deaths: 0,
+        deaths: 0,
         lastupdatedtime: "",
-//         recovered: 0,
+        recovered: 0,
         delta: {
           confirmed: 0
         }
@@ -37,13 +37,13 @@ try {
     if (isToday) {
       currentDistrict.delta.confirmed++;
     }
-//     if(row.currentstatus === 'Hospitalized') {
-//       currentDistrict.active++;
-//     } else if(row.currentstatus === 'Deceased') {
-//       currentDistrict.deaths++;
-//     } else if(row.currentstatus === 'Recovered') {
-//       currentDistrict.recovered++;
-//     }
+    if(row.currentstatus === 'Hospitalized') {
+      currentDistrict.active++;
+    } else if(row.currentstatus === 'Deceased') {
+      currentDistrict.deaths++;
+    } else if(row.currentstatus === 'Recovered') {
+      currentDistrict.recovered++;
+    }
 
     return acc;
   
